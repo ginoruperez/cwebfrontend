8a69e9c07d0e174aa17dcd813c9983d7cf909c99
ginoruperez <ginoruperez@gmail.com>
2022-12-26 20:02:44 +0000

    update for aws-deploy

---
+++ server/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm install
+npm install react-router-dom
+npm install bootstrap@5.2.0
+npm install @mui/material
+npm install bcrypt
+npm install @mui/x-data-grid
+npm install @emailjs/browser
+npm install -g json-server
+npm install @mui/icons-material
+npm start
